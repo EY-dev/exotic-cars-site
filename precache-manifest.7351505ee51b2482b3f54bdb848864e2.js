37efa73d6832aa7e952cb596c6408f370476b2e5
Eugene Yesaulov <enot8826@gmail.com>
2020-08-23 20:52:56 -0700

    Add files via upload

---
+++ precache-manifest.7351505ee51b2482b3f54bdb848864e2.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37b971f40c7a2440d8cb",
+    "url": "/css/app.84de188a.css"
+  },
+  {
+    "revision": "9c4375a4d8b91dee2c97",
+    "url": "/css/chunk-vendors.406b296e.css"
+  },
+  {
+    "revision": "f82658126329f7d1a64587372a8d4ef7",
+    "url": "/img/aventador.jpg"
+  },
+  {
+    "revision": "19cf5afa331b2a99488cf6d276ceb868",
+    "url": "/img/car.jpg"
+  },
+  {
+    "revision": "7e574d553ee55736869489d6e7a9bb3a",
+    "url": "/img/cars/aventador_1.jpg"
+  },
+  {
+    "revision": "c4b0d4fe127ce426812efa49da0ea3e2",
+    "url": "/img/cars/aventador_2.jpg"
+  },
+  {
+    "revision": "c80ababa0af287312c1867a4792d7a96",
+    "url": "/img/cars/aventador_3.jpg"
+  },
+  {
+    "revision": "d2c5b00127faf6a969c45f75eebb63ff",
+    "url": "/img/cars/bmw_1.jpg"
+  },
+  {
+    "revision": "d69afa93df5483f2e316ec01a4e4cd19",
+    "url": "/img/cars/bmw_2.jpg"
+  },
+  {
+    "revision": "d51f24f90f96fa417edd09f418aeb466",
+    "url": "/img/cars/bmw_3.jpg"
+  },
+  {
+    "revision": "7531b163738d630d0330c504040bb3be",
+    "url": "/img/cars/ferrari1.jpg"
+  },
+  {
+    "revision": "3ff1f8514c42e5f0b1988c01f4c354fb",
+    "url": "/img/cars/ferrari2.jpg"
+  },
+  {
+    "revision": "1d6ddcbb187ff028a7d033382ccc5283",
+    "url": "/img/cars/ferrari3.jpg"
+  },
+  {
+    "revision": "b5146094b6f793f15f57ea022851435e",
+    "url": "/img/cars/mclaren_1.jpg"
+  },
+  {
+    "revision": "9691444504affd9f027ba38dc0a4dd30",
+    "url": "/img/cars/mclaren_2.jpg"
+  },
+  {
+    "revision": "de36d73f864c5de934fa4604e0fc325a",
+    "url": "/img/cars/mclaren_3.jpg"
+  },
+  {
+    "revision": "6deb80d3ea0cc1d9eaef006640151133",
+    "url": "/img/cars/mclaren_4.jpg"
+  },
+  {
+    "revision": "231a64e4ffd343a957e9c878debdacf3",
+    "url": "/img/cars/porsche_1.jpg"
+  },
+  {
+    "revision": "28a1e3616ac6795ba10c48e35a222dd7",
+    "url": "/img/cars/porsche_2.jpg"
+  },
+  {
+    "revision": "609c1bced8d54b70f1deb10fe68e02f3",
+    "url": "/img/cars/porsche_3.jpg"
+  },
+  {
+    "revision": "2c62b55a8afcb74fa605773996526952",
+    "url": "/img/cars/porsche_4.jpg"
+  },
+  {
+    "revision": "63e85afc01c3af185e56454b6fab3339",
+    "url": "/img/cars/porsche_5.jpg"
+  },
+  {
+    "revision": "b907c04a0d66604c13077ea3f779d3f2",
+    "url": "/img/cars/tomaso_1.jpg"
+  },
+  {
+    "revision": "f0e56aaebe0773e845fd70373609146c",
+    "url": "/img/cars/tomaso_2.jpg"
+  },
+  {
+    "revision": "3f1663ce5ee7b3d614af26dc0e4c0a19",
+    "url": "/img/cars/tomaso_3.jpg"
+  },
+  {
+    "revision": "f4f47eee4fa331e74112211fef66b794",
+    "url": "/img/cars/tomaso_4.jpg"
+  },
+  {
+    "revision": "2d8dafc0f25fe7f91a3bea5aee5f2297",
+    "url": "/img/gallardo.jpg"
+  },
+  {
+    "revision": "5f910fd9684343caa515cc37317569a1",
+    "url": "/img/snazzy-image.jpg"
+  },
+  {
+    "revision": "368cddbf5c26de7166ed52636b8b1a37",
+    "url": "/img/snazzy-image.png"
+  },
+  {
+    "revision": "37ad504060f914971039b2a1d0e762f9",
+    "url": "/img/tablet-template.svg"
+  },
+  {
+    "revision": "c1b0e20aa7ac75de7f5904bff2a9885c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37b971f40c7a2440d8cb",
+    "url": "/js/app.6abf4da7.js"
+  },
+  {
+    "revision": "9c4375a4d8b91dee2c97",
+    "url": "/js/chunk-vendors.7957b87a.js"
+  },
+  {
+    "revision": "6c356a4d8e4d55eef861c704fd15e8ed",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
